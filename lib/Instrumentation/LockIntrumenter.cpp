c4966663bc3ca2111b9a27426a91e41302ac8fe5
Ali Kheradmand <a.i.kheradmand@gmail.com>
2013-08-22 21:32:42 +0200

    LockInstumenter passes debug info to Tracer. Tracer now builds runtime address order graph

--- lib/Instrumentation/LockIntrumenter.cpp
+++ lib/Instrumentation/LockIntrumenter.cpp
@@ -6,9 +6,9 @@
  */
 
 #define INITIALIZE "_Z10initializev"
-#define BEFORE_LOCK "_Z10beforeLockP15pthread_mutex_t"
-#define AFTER_LOCK "_Z9afterLockP15pthread_mutex_t"
-#define AFTER_UNLOCK "_Z11afterUnlockP15pthread_mutex_t"
+#define BEFORE_LOCK "_Z10beforeLockP15pthread_mutex_ti"
+#define AFTER_LOCK "_Z9afterLockP15pthread_mutex_ti"
+#define AFTER_UNLOCK "_Z11afterUnlockP15pthread_mutex_ti"
 #define FINALIZE "_Z8finalizev"
 
 
@@ -17,26 +17,66 @@
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/DerivedTypes.h"
 #include "llvm/IR/GlobalValue.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Constants.h"
 
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/Debug.h"
 
+#include "llvm/DebugInfo.h"
 #include "llvm/Pass.h"
 
+#include <map>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+
 using namespace llvm;
+using namespace std;
 
 class LockInstrumenter : public ModulePass{
 public:
 	static char ID;
+	typedef map<string, int> LocationIndexMap;
 
 	LockInstrumenter() : ModulePass(ID) {
-
+		locNum = 0;
 	}
+
+
+	LocationIndexMap locIndex;
+	int locNum;
+	int getLocationIndex(const Instruction* inst){
+		MDNode *node = inst->getDebugLoc().getAsMDNode(inst->getContext());
+		DILocation loc(node);
+
+		unsigned line = loc.getLineNumber();
+		StringRef file = loc.getFilename();
+
+		stringstream locStream;
+		locStream << line;
+		string locStr = locStream.str();
+		string location(file.str() + ":" + locStr);
+
+		if (locIndex.find(location) == locIndex.end())
+			locIndex[location] = locNum++;
+
+		return locIndex[location];
+	}
+
+	void printLocationIndexMap(){
+		ofstream fout("location-index-map.txt");
+		for (LocationIndexMap::iterator it = locIndex.begin(); it != locIndex.end(); it++)
+			fout <<  it->first << "\t" << it->second << endl;
+		fout.close();
+	}
+
 
 	virtual bool runOnModule(Module &m) {
 		Type* mutex_t = m.getTypeByName("union.pthread_mutex_t");
 		assert(mutex_t && "there is no lock in this program!\n");
-		Type* params[] = { PointerType::getUnqual(mutex_t) };
+		Type* params[] = { PointerType::getUnqual(mutex_t), IntegerType::getInt32Ty(getGlobalContext()) };
 		FunctionType* funcType = FunctionType::get(Type::getVoidTy(getGlobalContext()), params, false);
 
 		Function* beforeLock = Function::Create(funcType, GlobalVariable::ExternalLinkage, BEFORE_LOCK, &m);
@@ -54,21 +94,34 @@ public:
 					if (CallInst* callInst = dyn_cast<CallInst>(inst)){
 						//errs() << "call to " << callInst->getCalledFunction()->getName() << "\n";
 						if (callInst->getCalledFunction()->getName() == "pthread_mutex_lock"){
+
 							DEBUG_WITH_TYPE("lock-instrument",
 									errs() << "+ call to pthread lock " << *callInst << "\n");
-							Value* args[] = { callInst->getArgOperand(0) };
+
+							Value* args[] = { callInst->getArgOperand(0),
+									ConstantInt::get(
+											Type::getInt32Ty(getGlobalContext()), getLocationIndex(inst))
+							};
 
 							CallInst* callBeforeLock = CallInst::Create(beforeLock, args);
 							CallInst* callAfterLock = CallInst::Create(afterLock, args);
 
 							callBeforeLock->insertBefore(inst);
 							callAfterLock->insertAfter(inst);
+
 						}else if (callInst->getCalledFunction()->getName() == "pthread_mutex_unlock"){
+
 							DEBUG_WITH_TYPE("lock-instrument",
 									errs() << "- call to pthread unlock " << *callInst << "\n");
-							Value* args[] = { callInst->getArgOperand(0) };
+
+							Value* args[] = { callInst->getArgOperand(0),
+									ConstantInt::get(
+											Type::getInt32Ty(getGlobalContext()), getLocationIndex(inst))
+							};
+
 							CallInst* callAfterUnlock = CallInst::Create(afterUnlock, args);
 							callAfterUnlock->insertAfter(inst);
+
 						}
 					}
 				}
@@ -87,6 +140,8 @@ public:
 		callInitialize->insertBefore(&firstInst);
 		//TODO: this is silly!
 		callFinalize->insertBefore(&lastInst);
+
+		printLocationIndexMap();
 
 		return true;
 	}
--- tools/LockTracer/LockTracer.cpp
+++ tools/LockTracer/LockTracer.cpp
@@ -10,45 +10,227 @@
 #include <pthread.h>
 #include <iostream>
 #include <vector>
+#include <map>
 #include <cstdio>
+#include <assert.h>
+#include <stdint.h>
+#include <fstream>
 using namespace std;
 
 
-vector<pthread_t> threads;
+namespace LockTracer{
 
-int getIndex(const pthread_t &t){
-	for (int i=0; i < (int)threads.size(); i++){
-		if (pthread_equal(threads[i], t) != 0)
-			return i;
+typedef int32_t Location;
+typedef pthread_mutex_t* LockAddress;
+typedef pair< Location , LockAddress> LockOperation;
+
+pthread_mutex_t tracerBigLock;
+
+ofstream log("trace.log");
+
+typedef map< LockAddress, int> AdjList;
+map<LockAddress, AdjList> RAOG; //runtime address order graph
+
+map<Location, string> locationString;
+
+class Thread{
+public:
+	static Thread* self(){
+		const pthread_t me = pthread_self();
+		int len = threads.size();
+		// it is OK if another thread push to vector, because that is definitely another thread.
+		for (int i=0; i < len; i++){
+			if (pthread_equal((threads[i]->pthread), me) != 0){
+				return threads[i];
+			}
+		}
+
+		//right now we lock at the beginning in order to keep everything simple
+		//pthread_mutex_lock(&tracerLock);;
+		threads.push_back(new Thread(me, threads.size()));
+		//pthread_mutex_unlock(&tracerLock);
+
+		return threads.back();
 	}
-	threads.push_back(t);
-	return threads.size()-1;
+
+	int getIndex(){
+		assert(index != -1 && "thread index can not be -1\n");
+		return index;
+	}
+
+	void lock(const LockOperation &lockAq){
+		if (!locks.empty()){
+			//add edge in runtime location order graph from all last locks to this lock
+			LockAddress to = lockAq.second;
+			for (typeof(locks.begin()) it = locks.begin(); it != locks.end(); it++){
+				LockAddress from = locks.back().second;
+				if (RAOG.find(from) == RAOG.end())
+					RAOG[from] = AdjList();
+				if (RAOG[from].find(to) == RAOG[from].end())
+					RAOG[from][to] = 0;
+				RAOG[from][to]++;
+			}
+		}
+		locks.push_back(lockAq);
+		log << "thread " << getIndex() << " locked " << lockAq.second << " at " << locationString[lockAq.first] << endl;
+
+	}
+
+	void unlock(const LockOperation &lockRel){
+		locks.pop_back();
+		log << "thread " << getIndex() << " unlocked " << lockRel.second << " at " << locationString[lockRel.first] << endl;
+	}
+
+private:
+	static vector<Thread*> threads;
+	const pthread_t pthread;
+	int index;
+	vector<LockOperation> locks;
+	Thread(const pthread_t &p, int idx):pthread(p), index(idx){
+	}
+};
+
+
+
+void printRuntimeAddressOrderGraph(){
+	ofstream fout("runtime-address-order-graph.txt");
+	fout << RAOG.size() << endl;
+	for (typeof(RAOG.begin()) n = RAOG.begin(); n != RAOG.end(); n++){
+		fout << n->first << " " << n->second.size() << endl;
+		for (AdjList::iterator e = n->second.begin(); e != n->second.end(); e++){
+			fout << "\t" << e->first << " " << e->second ;
+		}
+		fout << endl;
+	}
+	fout.close();
+}
+
+void logRuntimeAddressOrderGraph(){
+	log << RAOG.size() << endl;
+	for (typeof(RAOG.begin()) n = RAOG.begin(); n != RAOG.end(); n++){
+		log << n->first << " " << n->second.size() << endl;
+		for (AdjList::iterator e = n->second.begin(); e != n->second.end(); e++){
+			log << "\t" << e->first << " " << e->second ;
+		}
+		log << endl;
+	}
+}
+
+void readRuntimeAddressOrderGraph(){
+	ifstream fin("runtime-address-order-graph.txt");
+
+	int nodes;
+	if (fin >> nodes){
+		for (int i = 0; i < nodes; i++){
+			void* temp;
+			LockAddress n;
+			int neibs;
+			fin >> temp >> neibs;
+			n = (LockAddress)temp;
+			RAOG[n] = AdjList();
+			for (int j = 0; j < neibs; j++){
+				LockAddress m;
+				int w;
+				fin >> temp >> w;
+				m = (LockAddress)temp;
+				RAOG[n][m] = w;
+			}
+		}
+	}
+
+	fin.close();
+	log << "initial runtime location order graph:\n";
+	logRuntimeAddressOrderGraph();
 }
 
 
+void initLocationStringMap(){
+	ifstream fin("location-index-map.txt");
+	string str;
+	while (fin >> str){
+		Location loc;
+		fin >> loc;
+		locationString[loc] = str;
+	}
+	fin.close();
+}
+
+//class Graph{
+//public:
+//
+//	class Node{
+//	public:
+//		Location value;
+//
+//
+//	};
+//};
+
+
+vector<Thread*> Thread::threads = vector<Thread*>();
 
 
 void initialize(){
 	printf("tracer started\n");
-	//cout << "tracer started" << endl;
+	pthread_mutex_init(&tracerBigLock, NULL);
+	initLocationStringMap();
+	readRuntimeAddressOrderGraph();
+
+
 }
 
-void beforeLock(pthread_mutex_t *m){
-	printf("thread %d is going to lock %p\n", getIndex(pthread_self()), m);
-	//cout << "thread " << getIndex(pthread_self()) << " is going to lock " << m << endl;
+void beforeLock(pthread_mutex_t *m, Location loc){
+	printf("%d: thread %d is going to lock %p\n", loc, Thread::self()->getIndex(), m);
 }
 
-void afterLock(pthread_mutex_t *m){
-	printf("thread %d is locked %p\n", getIndex(pthread_self()), m);
-	//cout << "thread " << getIndex(pthread_self()) << " locked " << m << endl;
+void afterLock(pthread_mutex_t *m, Location loc){
+	printf("%d: thread %d locked %p\n", loc, Thread::self()->getIndex(), m);
+	Thread::self()->lock(LockOperation(loc, m));
 }
 
-void afterUnlock(pthread_mutex_t *m){
-	printf("thread %d released %p\n", getIndex(pthread_self()), m);
-	//cout << "thread " << getIndex(pthread_self()) << " released " << m << endl;
+void afterUnlock(pthread_mutex_t *m, Location loc){
+	printf("%d: thread %d released %p\n", loc, Thread::self()->getIndex(), m);
+	Thread::self()->unlock(LockOperation(loc, m));
 }
 
 void finalize(){
 	printf("tracer ended\n");
-	//cout << "tracer ended" << endl;
+
+	log << "final run time location order graph:" << endl;
+	logRuntimeAddressOrderGraph();
+
+	printRuntimeAddressOrderGraph();
+	log.close();
+}
+
+
+}
+
+/// Wrappers for linking purposes
+
+void initialize(){
+	LockTracer::initialize();
+}
+
+void beforeLock(pthread_mutex_t *m, int32_t loc){
+	pthread_mutex_lock(&LockTracer::tracerBigLock);
+	LockTracer::beforeLock(m, loc);
+	pthread_mutex_unlock(&LockTracer::tracerBigLock);
+	//pthread_yield();
+}
+
+void afterLock(pthread_mutex_t *m, int32_t loc){
+	pthread_mutex_lock(&LockTracer::tracerBigLock);
+	LockTracer::afterLock(m, loc);
+	pthread_mutex_unlock(&LockTracer::tracerBigLock);
+}
+
+void afterUnlock(pthread_mutex_t *m, int32_t loc){
+	pthread_mutex_lock(&LockTracer::tracerBigLock);
+	LockTracer::afterUnlock(m, loc);
+	pthread_mutex_unlock(&LockTracer::tracerBigLock);
+}
+
+void finalize(){
+	LockTracer::finalize();
 }
